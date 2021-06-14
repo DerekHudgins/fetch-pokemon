9e14ce246a3fe1c92f4fd6f30ed4692fae6fb103
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-14 10:01:58 -0700

    changed loading gif

--- src/Loading.js
+++ src/Loading.js
@@ -5,7 +5,7 @@ export default class Loading extends Component {
       <p>
         <div> LOADING
           </div>
-        <img src="https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif" alt="load" />
+        <img src="https://i.imgur.com/uhSWYG2.gif" alt="load" />
       </p>
     )
   }
